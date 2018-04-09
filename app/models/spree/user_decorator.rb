1b84e803d9c8235574ef0962e6d2ce309d9db713
MohamedHegab <mohamedhegab92@gmail.com>
2018-04-09 21:39:43 +0200

    removing spree

--- app/models/spree/user_decorator.rb
+++
@@ -1,8 +0,0 @@
-Spree::User.class_eval do 
-	def generate_jwt
-	  JWT.encode({ id: id,
-	              exp: 60.days.from_now.to_i },
-	             Rails.application.secrets.secret_key_base)
-	end
-
-end

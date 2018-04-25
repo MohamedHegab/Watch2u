b2716c3f51c4d8a79845ba04a06c466ab148d29c
MohamedHegab <mohamedhegab92@gmail.com>
2018-04-25 12:38:06 +0200

    return region sign-in

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -7,7 +7,7 @@ class Api::V1::SessionsController < Api::BaseController
       @user.generate_authentication_token!
       @user.save
       # render json: {status: 'success', data: user}
-      render_success(nil, :ok, "user signed in", @user)
+      render_success(:show, :ok, "user signed in", @user, no_meta: true)
     else
       render_fail('invalid email or password')
     end
@@ -18,7 +18,7 @@ class Api::V1::SessionsController < Api::BaseController
     if current_user
       current_user.generate_authentication_token!
       current_user.save
-      render_success(nil, 204, "user signed out succefully", current_user)
+      render_success(nil, 204, "user signed out successfully", current_user)
       # render json: {status: 'success', data: current_user}
     else
       # render_fail('There user not signed in')
